c571f525c94e30bf7dbd5e0c3a9b2360bb5e20b8
shilpasantpure <santpureshilpa@gmail.com>
2020-06-02 00:41:33 +0530

    Updates

---
+++ precache-manifest.9be3a5e44a9817a40e96c46e774e1a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ae72a8503b8f09df1aeb1bc06ab7e1",
+    "url": "/sample-react-portfolio/index.html"
+  },
+  {
+    "revision": "8b0d1e98cf8365b63a3c",
+    "url": "/sample-react-portfolio/static/css/main.5c3aa331.chunk.css"
+  },
+  {
+    "revision": "8e9eb092683dbc03780c",
+    "url": "/sample-react-portfolio/static/js/2.9c1d0159.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-react-portfolio/static/js/2.9c1d0159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0d1e98cf8365b63a3c",
+    "url": "/sample-react-portfolio/static/js/main.59d0336e.chunk.js"
+  },
+  {
+    "revision": "b30c85ad2f715d18b815",
+    "url": "/sample-react-portfolio/static/js/runtime-main.7ab753fc.js"
+  }
+]);
